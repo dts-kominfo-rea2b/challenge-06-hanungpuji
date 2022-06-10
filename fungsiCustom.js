7e0cdb850dea189ec1df62af743a925a32edd49b
hanungpuji <hanung@ilcs.co.id>
2022-06-10 20:03:11 +0700

    callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs')
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,79 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+let isiArrayFile = [];
+
+const loopingObject = (dataObject) => {
+  let lengthData = dataObject.length;
+  if (lengthData == undefined) {
+    lengthData = 0;
+  }
+
+  for (let i = 0; i <= lengthData; i++) {
+    if (typeof(dataObject) === 'object' && lengthData == 0) {
+      return dataObject[Object.keys(dataObject)]
+    }else if(typeof(dataObject) === 'object' && lengthData > 0){
+      let dataObject2 = dataObject[Object.keys(dataObject)]
+      if (dataObject2.length === 0) {
+        let objectKey = Object.keys(dataObject2);
+        return dataObject2[objectKey];
+      }else{
+        dataObject2 = dataObject2[Object.keys(dataObject2)]
+        if(typeof(dataObject2) === 'object' && lengthData > 0){
+          lengthData = dataObject2.length || 0
+          for (let i = 0; i <= lengthData; i++) {
+            objectKey = Object.keys(dataObject2);
+            return dataObject2[objectKey];
+          }
+        }else{
+          return dataObject2;
+        }
+        
+      }
+    }
+  
+  }
+  
+}
+
+const parseData = (isiText) => {
+  const data = JSON.parse(isiText);
+  const resultLoop = loopingObject(data)
+  return splitData(resultLoop)
+}
+
+const splitData = (isiData) => {
+  const hasil = isiData.split(' ');
+  return hasil[1];
+}
+
+const bacaFile = (file, index, array) => {
+  fs.readFile(file, 'utf8', function(err, data){
+    if (err) {
+      console.log('error:' + err)
+    }else{
+      const hasilParse = parseData(data);
+    }  
+  });
+}
+
+const bacaData = (fnCallback) => {  
+  const arrayFile = [file1,file2,file3];
+  arrayFile.forEach((file, index, array) => { 
+    fs.readFile(file, 'utf8', function(err, data){
+      if (err) {
+        console.log('error'+ err);
+      }else{
+        const hasilParse = parseData(data);
+        // array[index] = hasilParse
+        isiArrayFile.push(hasilParse);
+        if (index == (arrayFile.length-1)) {
+          fnCallback(err,isiArrayFile);
+        }
+      }  
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
